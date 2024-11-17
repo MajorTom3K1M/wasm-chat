74a77d0b01b513e29b0d1db3cbb4b4e8182e27f4
MajorTom <invaderzgame@gmail.com>
2024-11-17 16:36:00 +0700

    [ADD] Fix unworking feature and change UI

--- src/lib.rs
+++ src/lib.rs
@@ -10,29 +10,35 @@ extern crate yew;
 #[macro_use]
 extern crate log;
 #[macro_use]
-extern crate stdweb;
+// extern crate stdweb;
 
 extern crate web_sys;
 
 extern crate js_sys;
 
-use services::PubnubService;
+extern crate serde_wasm_bindgen;
+
+extern crate wasm_bindgen_futures;
+
+use services::{HereNowResponse, PubnubService};
 use std::collections::HashSet;
+use wasm_bindgen::JsValue;
+use web_sys::{HtmlInputElement, InputEvent, KeyboardEvent};
 use yew::prelude::*;
-use web_sys::{ InputEvent, KeyboardEvent, HtmlInputElement };
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Message {
     pub text: String,
-    pub from: String
+    pub from: String,
 }
 
 pub struct Model {
     alias: String,
+    thisUser: String,
     pending_text: String,
     messages: Vec<Message>,
     users: HashSet<String>,
-    pubnub: PubnubService
+    pubnub: PubnubService,
 }
 
 #[derive(Debug)]
@@ -44,7 +50,9 @@ pub enum Msg {
     UserOffline(String),
     UserOnline(String),
     UpdatePendingText(String),
-    NoOp
+    FetchHereNow,
+    HereNowFetched(Result<HereNowResponse, JsValue>),
+    NoOp,
 }
 
 impl Component for Model {
@@ -54,55 +62,83 @@ impl Component for Model {
     fn create(_ctx: &Context<Self>) -> Self {
         Model {
             messages: Vec::new(),
+            thisUser: "".to_owned(),
             alias: "".to_owned(),
             users: HashSet::new(),
             pending_text: "".to_owned(),
-            pubnub: PubnubService::new()
-        }
+            pubnub: PubnubService::new(),
+        } 
     }
 
     fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
+        info!("Update: {:?}", msg);
         match msg {
             Msg::AddMessage(msg) => {
                 self.messages.push(msg);
-            },
+            }
             Msg::UserOnline(nick) => {
                 info!("Adding user {:?}", nick);
                 self.users.insert(nick);
-            },
+            }
             Msg::UserOffline(nick) => {
                 info!("Removing user {:?}", nick);
                 self.users.remove(&nick);
-            },
+            }
             Msg::SendChat => {
                 info!("Called send chat!");
                 if self.pending_text != "" {
                     self.pubnub.send_message(&self.pending_text);
                     self.pending_text = "".to_owned();
                 }
-            },
+            }
             Msg::Connect => {
                 info!("Connected");
-                
+
                 let on_message = ctx.link().callback(|msg| Msg::AddMessage(msg));
                 let onoffline = ctx.link().callback(|user| Msg::UserOffline(user));
                 let ononline = ctx.link().callback(|user| Msg::UserOnline(user));
-                
+                self.thisUser = self.alias.clone();
+
                 self.pubnub.connect(
                     "chatengine-demo-chat",
-                    &self.alias, 
-                    on_message, 
-                    onoffline, 
-                    ononline
+                    &self.alias,
+                    on_message,
+                    onoffline,
+                    ononline,
                 );
-            },
+
+                ctx.link().send_message(Msg::FetchHereNow);
+            }
             Msg::EnterName(n) => {
-                info!("Enter Name {:?}", n);
                 self.alias = n;
-            },
+            }
             Msg::UpdatePendingText(s) => {
                 self.pending_text = s;
-            },
+            }
+            Msg::FetchHereNow => {
+                let callback = ctx.link().callback(|response| Msg::HereNowFetched(response));
+                let params = services::HereNowParameters {
+                    channels: vec!["chatengine-demo-chat".to_owned()],
+                    include_uuids: true,
+                    include_state: false,
+                };
+
+                self.pubnub.fetch_here_now(params, callback);
+            }
+            Msg::HereNowFetched(Ok(response)) => {
+                info!("HereNowFetched: {:?}", response);
+                response.channels.unwrap().get("chatengine-demo-chat").map(|channel| {
+                    channel.occupants.iter().for_each(|occupant| {
+                        info!("Occupant: {:?}", occupant);
+                        for occ in occupant.iter() {
+                            ctx.link().send_message(Msg::UserOnline(occ.uuid.clone()));
+                        }
+                    }); 
+                });
+            }
+            Msg::HereNowFetched(Err(e)) => {
+                error!("Error fetching here now: {:?}", e);
+            }
             Msg::NoOp => {}
         }
         true
@@ -110,68 +146,135 @@ impl Component for Model {
 
     fn view(&self, ctx: &Context<Self>) -> Html {
         html! {
-            <div class="wrapper">
-                <div class="chat-text">
-                    <h1>{ "Messages" }</h1><br/>
-                    <ul class="message-list">
-                        { for self.messages.iter().enumerate().map(view_message) }
-                    </ul>
+            <div class="flex-container">
+                <div class="chat-container">
+                    <div class="chat-header">
+                        <h1 class="chat-header-title">{"Chat Room"}</h1>
+                        <button class="chat-header-button">
+                            <i class="chat-header-icon fa-regular fa-user"></i>
+                        </button>
+                    </div>
+
+                    <div class="chat-messages-container">
+                        <div class="messages-space">
+                            { for self.messages.iter().enumerate().map(|(idx, message)| {
+                                view_message((idx, message, self.thisUser.as_str()))
+                            }) }
+                        </div>
+                    </div>
+
+                    <div class="input-container">
+                        <div class="connect-form" aria-label="Connect to Chat">
+                            <input
+                                type="text"
+                                placeholder="Enter your username"
+                                class="connect-input"
+                                value={ self.alias.to_string() }
+                                oninput={ ctx.link().callback(|e: InputEvent| {
+                                    let input: HtmlInputElement = e.target_unchecked_into();
+                                    Msg::EnterName(input.value())
+                                }) }
+                                disabled={ !self.thisUser.is_empty() }
+                            />
+
+                            <button
+                                class="connect-button"
+                                disabled={ !self.thisUser.is_empty() }
+                                onclick={ ctx.link().callback(|_| Msg::Connect) }
+                            >
+                                { if self.thisUser.is_empty() { "Connect" } else { "Connected" } }
+                            </button>
+                        </div>
+
+                        <div class="connect-form" aria-label="Connect to Chat">
+                            <input
+                                type="text"
+                                placeholder="Type a message..."
+                                class="connect-input"
+                                disabled={ self.thisUser.is_empty() }
+                                value={ self.pending_text.to_string() }
+                                oninput={ ctx.link().callback(|e: InputEvent| {
+                                    let input: HtmlInputElement = e.target_unchecked_into();
+                                    Msg::UpdatePendingText(input.value())
+                                }) }
+                                onkeypress={ ctx.link().callback(|e: KeyboardEvent| {
+                                    if e.key() == "Enter" {
+                                        Msg::SendChat
+                                    } else {
+                                        Msg::NoOp
+                                    }
+                                }) }
+                            />
+
+                            <button
+                                class="send-button"
+                                disabled={ self.thisUser.is_empty() }
+                                onclick={ ctx.link().callback(|_| Msg::SendChat) }
+                            >
+                                <i class="send-icon fa-regular fa-paper-plane"></i>
+                                {"Send"}
+                            </button>
+                        </div>
+                    </div>
                 </div>
-                <div class="users">
-                    <h1>{ "Users" }</h1>
-                    <ul class="user-list">
+
+                <div class="users-container">
+                    <div class="users-header">
+                        <h2 class="text-title">{"Users"}</h2>
+                    </div>
+                    <div class="users-list">
                         { for self.users.iter().enumerate().map(view_user) }
-                    </ul>
+                        <div class="separator"></div>
+                    </div>
                 </div>
-                <div class="connect">
-                    <input placeholder="Your Name" 
-                        value={ self.alias.to_string() }  
-                        oninput={ ctx.link().callback(|e: InputEvent| { 
-                            let input: HtmlInputElement = e.target_unchecked_into();
-                            Msg::EnterName(input.value())
-                        }) }
-                    />
-                    <button onclick={ ctx.link().callback(|_| Msg::Connect) }>{ "Connect" }</button>
+            </div>
+        }
+    }
+
+    fn destroy(&mut self, ctx: &Context<Self>) {
+        println!("Destroying");
+        self.pubnub.disconnect();
+    }
+}
+
+fn view_message((_idx, message, this_user): (usize, &Message, &str)) -> Html {
+    if this_user == message.from {
+        html! {
+            <div class="message sent">
+                <div class="avatar">
+                    <img src="https://via.placeholder.com/40" alt="You" class="avatar-image"/>
+                    <div class="avatar-fallback">{"Y"}</div>
                 </div>
-                <div class="text-entry">
-                    <input 
-                        placeholder="Message Text"
-                        class="pending-text"
-                        value={ self.pending_text.to_string() }
-                        oninput={ ctx.link().callback(|e: InputEvent| { 
-                            let input: HtmlInputElement = e.target_unchecked_into();
-                            Msg::UpdatePendingText(input.value())
-                        }) }
-                        onkeypress={ ctx.link().callback(|e: KeyboardEvent| {
-                            if e.key() == "Enter" {
-                                Msg::SendChat
-                            } else {
-                                Msg::NoOp
-                            }
-                        }) }
-                    />
+                <div class="message-content">
+                    <p class="message-user">{&message.from}</p>
+                    <p class="message-text">{&message.text}</p>
+                </div>
+            </div>
+        }
+    } else {
+        html! {
+            <div class="message received">
+                <div class="avatar">
+                    <img src="https://via.placeholder.com/40" alt="User" class="avatar-image" />
+                    <div class="avatar-fallback">{"U"}</div>
+                </div>
+                <div class="message-content">
+                    <p class="message-user">{&message.from}</p>
+                    <p class="message-text">{&message.text}</p>
                 </div>
             </div>
         }
     }
 }
 
-fn view_message((_idx, message): (usize, &Message)) -> Html {
-    html! {
-        <li>
-            <label>
-                <span class="sender">{"["}{&message.from}{"]"}</span>
-                <span class="chatmsg">{&message.text}</span>
-            </label>
-        </li>
-    }
-}
-
 fn view_user((_idx, user): (usize, &String)) -> Html {
     html! {
-        <li>
-            <label>{ user }</label>
-        </li>
+        <div class="user">
+            <div class="avatar">
+                <img src="https://via.placeholder.com/40" class="avatar-image" />
+            </div>
+            <div>{user}</div>
+        </div>
     }
 }
 
--- src/services.rs
+++ src/services.rs
@@ -1,9 +1,20 @@
 use super::Message;
+use js_sys::Date;
+use js_sys::Math::random;
+use js_sys::Promise;
 use js_sys::JSON;
-use stdweb::Value;
-use web_sys::console::info;
-use yew::prelude::*;
+use serde_wasm_bindgen::from_value;
+use serde_wasm_bindgen::to_value;
+// use stdweb::Value;
 use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
+use web_sys::console::info;
+use web_sys::console::time_stamp;
+use yew::prelude::*;
+use std::collections::HashMap;
+use std::rc::Rc;
+use std::thread;
+use std::time::Duration;
 
 #[wasm_bindgen]
 extern "C" {
@@ -21,11 +32,26 @@ extern "C" {
     #[wasm_bindgen(method)]
     fn publish(this: &PubNub, payload: JsValue);
 
+    #[wasm_bindgen(method, structural, js_name = "unsubscribe")]
+    fn unsubscribe(this: &PubNub, channels: JsValue);
+
+    #[wasm_bindgen(method)]
+    fn unsubscribeAll(this: &PubNub);
+
     #[wasm_bindgen(method, structural)]
     fn setUUID(this: &PubNub, uuid: String);
 
     #[wasm_bindgen(method, structural)]
     fn getSubscribedChannels(this: &PubNub) -> js_sys::Array;
+
+    #[wasm_bindgen(method, structural, js_name = "objects.getAllUUIDMetadata")]
+    fn get_all_uuid_metadata(this: &PubNub, params: JsValue) -> Promise;
+
+    #[wasm_bindgen(method, structural, js_name = "objects.removeUUIDMetadata")]
+    fn remove_uuid_metadata(this: &PubNub, params: JsValue) -> Promise;
+
+    #[wasm_bindgen(method, structural, js_name = "hereNow")]
+    fn here_now(this: &PubNub, params: JsValue) -> Promise;
 }
 
 #[derive(Serialize)]
@@ -33,58 +59,106 @@ extern "C" {
 pub struct Initialize {
     pub publish_key: String,
     pub subscribe_key: String,
+    pub heartbeat_interval: u32,
 }
 pub struct PubnubService {
-    lib: Option<PubNub>,
-    chat: Option<Value>,
-    channel: Option<String>
+    lib: Rc<PubNub>,
+    // chat: Option<Value>,
+    channel: Option<String>,
+    username: Option<String>,
 }
 
 #[derive(Serialize)]
 pub struct Channels {
     pub channels: Vec<String>,
-    pub withPresence: bool
+    pub withPresence: bool,
+}
+
+#[derive(Serialize)]
+pub struct UnsubscribeChannels {
+    pub channels: Vec<String>,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
 struct EventStatus {
     pub category: String,
-    pub operation: String
+    pub operation: String,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct PubNubMessage {
     channel: String,
     publisher: String,
-    message: String
+    message: String,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Presence {
-    pub uuid: String
+    pub uuid: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct HereNowParameters {
+    pub channels: Vec<String>,
+    #[serde(rename = "includeUUIDs")]
+    pub include_uuids: bool,
+    #[serde(rename = "includeState")]
+    pub include_state: bool,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct HereNowResponse {
+    #[serde(rename = "totalChannels")]
+    pub total_channels: Option<u32>,
+    #[serde(rename = "totalOccupancy")]
+    pub total_occupancy: Option<u32>,
+    pub channels: Option<std::collections::HashMap<String, ChannelOccupancy>>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ChannelOccupancy {
+    pub name: String,
+    pub occupancy: Option<u32>,
+    pub occupants: Option<Vec<Occupants>>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct  Occupants {
+    pub uuid: String,
+    pub state: Option<String>,
 }
 
 impl PubnubService {
     pub fn new() -> Self {
         let init = Initialize {
-            publish_key: "pub-c-c05c98be-8fc6-4a7f-8353-853b4157c8a4".to_owned(),
-            subscribe_key: "sub-c-79016488-a6ae-11ec-b6b1-728be7898cef".to_owned(),
+            publish_key: "pub-c-bda71cb0-c615-4fa3-9d91-da409870c219".to_owned(),
+            subscribe_key: "sub-c-0719525a-9783-4fd2-baf1-384eb78d5845".to_owned(),
+            heartbeat_interval: 6
         };
-        let chat_engine = PubNub::new(JsValue::from_serde(&init).unwrap());
+        // let chat_engine = PubNub::new(to_value(&init).unwrap());
+        let chat_engine = Rc::new(PubNub::new(to_value(&init).unwrap()));
+
+        PubnubService::setup_disconnect_handler(chat_engine.clone());
+
         PubnubService {
-            lib: Some(chat_engine),
-            chat: None,
-            channel: None
+            lib: chat_engine,
+            // chat: None,
+            channel: None,
+            username: None,
         }
     }
 
     pub fn send_message(&mut self, msg: &str) {
-        let lib = self.lib.as_ref().expect("No pubnub library!");
+        let lib = self.lib.as_ref();
 
-        let payload = JSON::parse(&format!(r#"{{
+        let payload = JSON::parse(&format!(
+            r#"{{
             "channel": "{}",
             "message": "{}"
-        }}"#, self.channel.as_ref().unwrap(), msg));
+        }}"#,
+            self.channel.as_ref().unwrap(),
+            msg
+        ));
 
         lib.publish(payload.unwrap());
     }
@@ -95,20 +169,21 @@ impl PubnubService {
         nickname: &str,
         onmessage: Callback<Message>,
         onoffline: Callback<String>,
-        ononline: Callback<String>
+        ononline: Callback<String>,
     ) -> () {
-        let lib = self.lib.as_ref().expect("No pubnub library!");
+        let lib = self.lib.as_ref();
 
-        lib.setUUID(nickname.to_string());
+        lib.setUUID(nickname.to_owned());
+        self.username = Some(nickname.to_owned());
 
         let chat_callback = move |text: String, source: String| {
             let msg = Message {
                 text: text,
-                from: source
+                from: source,
             };
             onmessage.emit(msg);
         };
-        let useroffline_callback = move | username: String | {
+        let useroffline_callback = move |username: String| {
             onoffline.emit(username);
         };
         let useronline_callback = move |username: String| {
@@ -120,38 +195,108 @@ impl PubnubService {
         if arr.index_of(&JsValue::from_str(topic), 0) < 0 {
             let channels = Channels {
                 channels: vec![topic.to_owned()],
-                withPresence: true
+                withPresence: true,
             };
             self.channel = Some(topic.to_owned());
+            
+            
+            lib.subscribe(to_value(&channels).unwrap());
 
-            lib.subscribe(JsValue::from_serde(&channels).unwrap());
-    
             let event_handler = js_sys::Object::new();
-            let _ = js_sys::Reflect::set(
-                &event_handler, 
-                &"status".into(), 
-                &Closure::wrap(Box::new(move |status_event: JsValue| { 
-                    let events: EventStatus = status_event.into_serde::<EventStatus>().unwrap();
-                    info!("{:?}", events.category == "PNConnectedCategory");
-                }) as Box<dyn FnMut(JsValue)>).into_js_value()
-            );
+
+            let message_closure = Closure::wrap(Box::new(move |message: JsValue| {
+                let msg: PubNubMessage = from_value(message).unwrap();
+                chat_callback(msg.message, msg.publisher);
+            }) as Box<dyn FnMut(JsValue)>);
             let _ = js_sys::Reflect::set(
                 &event_handler,
                 &"message".into(),
-                &Closure::wrap(Box::new(move |message: JsValue| {
-                    let msg: PubNubMessage = message.into_serde::<PubNubMessage>().unwrap();
-                    chat_callback(msg.message, msg.publisher);
-                }) as Box<dyn FnMut(JsValue)>).into_js_value()
+                &message_closure.as_ref().unchecked_ref(),
             );
+            message_closure.forget();
+
+            let presence_closure = Closure::wrap(Box::new(move |presence: JsValue| {
+                info!("Presence event: {:?}", presence);
+                let pres: Presence = from_value(presence).unwrap();
+                useronline_callback(pres.uuid);
+            }) as Box<dyn FnMut(JsValue)>);
             let _ = js_sys::Reflect::set(
                 &event_handler,
                 &"presence".into(),
-                &Closure::wrap(Box::new(move |presence: JsValue| {
-                    let pres: Presence = presence.into_serde::<Presence>().unwrap();
-                    useronline_callback(pres.uuid);
-                }) as Box<dyn FnMut(JsValue)>).into_js_value()
+                &presence_closure.as_ref().unchecked_ref(),
             );
+            presence_closure.forget();
+
             lib.addListener(event_handler);
         }
+    }
+
+    pub fn disconnect(&mut self) {
+        let lib = self.lib.as_ref();
+        self.username = None;
+        self.channel = None;
+        // lib.setUUID("Mike".to_owned());
+        let channels = UnsubscribeChannels {
+            channels: vec!["chatengine-demo-chat".to_owned()],
+        };
+        lib.unsubscribe(to_value(&channels).unwrap());
+    }
+
+    fn setup_disconnect_handler(lib: Rc<PubNub>) {
+        info!("Setting up disconnect handler");
+        let disconnect_closure = Closure::wrap(Box::new(move |_: Event| {
+            info!("Disconnecting");
+            lib.unsubscribeAll();
+        }) as Box<dyn FnMut(Event)>);
+
+        let window = web_sys::window().expect("no global `window` exists");
+
+        window
+            .add_event_listener_with_callback("beforeunload", disconnect_closure.as_ref().unchecked_ref())
+            .expect("failed to add unload event listener");
+
+        disconnect_closure.forget();
+        info!("Disconnect handler setup");
+    }
+
+    pub fn fetch_uuid_metadata(&self, callback: Callback<HashMap<String, String>>) {
+        let lib = self.lib.as_ref();
+
+        let params = js_sys::Object::new(); // Pass any required parameters
+        let promise = lib.get_all_uuid_metadata(params.into());
+
+        let closure = Closure::wrap(Box::new(move |result: JsValue| {
+            if let Ok(data) = from_value::<Vec<Presence>>(result) {
+                let mut metadata_map = HashMap::new();
+                for presence in data {
+                    metadata_map.insert(presence.uuid.clone(), presence.uuid.clone()); // Example
+                }
+                callback.emit(metadata_map);
+            }
+        }) as Box<dyn FnMut(JsValue)>);
+
+        let _ = promise.then(&closure);
+        closure.forget();
+    }
+
+    pub fn fetch_here_now(
+        &self,
+        params: HereNowParameters,
+        callback: Callback<Result<HereNowResponse, JsValue>>,
+    ) {
+        let lib = self.lib.as_ref();
+
+        let js_params = to_value(&params).expect("Failed to serialize HereNowParameters");
+        let promise = lib.here_now(js_params);
+
+        let closure = Closure::wrap(Box::new(move |result: JsValue| {
+            info!("HereNow response: {:?}", result);
+            let response: Result<HereNowResponse, JsValue> = from_value(result)
+                .map_err(|e| JsValue::from_str(&e.to_string()));
+            callback.emit(response);
+        }) as Box<dyn FnMut(JsValue)>);
+
+        let _ = promise.then(&closure);
+        closure.forget();
     }
 }
